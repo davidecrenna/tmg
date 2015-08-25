8a4567d64f247dddec85a2d670f6e054a9b96a62
davidecrenna <dvdcrenna@gmail.com>
2015-08-25 23:43:11 +0800

    SWITCHED PHPSTORM 9.0.2

--- annulla_payment.php
+++ annulla_payment.php
@@ -3,24 +3,22 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Iscrizione - Top Manager Group</title>
-</head>
-<body>
 <?php 
 require_once "headerbasic.php";
 include("header.php"); ?>
 
 <link rel="stylesheet" type="text/css" href="thank-you/css/thank-you.css"/>
-<script src="http://cdn.jquerytools.org/1.2.6/full/jquery.tools.min.js"></script>
+<script type="application/javascript" src="http://cdn.jquerytools.org/1.2.6/full/jquery.tools.min.js"></script>
 
 </head>
 
 <body>
 
-	<div align="center"><a href="index.php"><img src="image/banner/banner_scaled.png" border="0"/></a></div>
+	<div align="center"><a href="index.php"><img alt="Homepage." src="image/banner/banner_scaled.png" border="0"/></a></div>
 	<div align="center">
           <div class="cols" id="scheda1">
                       <div style="" class="col">
-                      		<p style="position:absolute; top:150px; width:900px; font-size:18px; font-family:Arial, Helvetica, sans-serif;" ><img src="image/icone/error.png" style="height:25px; width:30px; vertical-align:middle;"/> Hai annullato il pagamento.<br/>
+                      		<p style="position:absolute; top:150px; width:900px; font-size:18px; font-family:Arial, Helvetica, sans-serif;" ><img src="image/icone/error.png" style="height:25px; width:30px; vertical-align:middle;" alt="Errore"/> Hai annullato il pagamento.<br/>
 								  Puoi ripetere la registrazione cliccando sul tasto inizia della homepage.<br/>
 								  Grazie<br/>
 								  Lo staff di TMG<br/></p>
@@ -38,7 +36,7 @@ include("header.php"); ?>
                
                   
                <div id="card_down">
-                <a href="index.php"><img src="image/btn/btn_torna_alla_home.png" onmouseover="Javascript:this.src='image/btn/btn_torna_alla_home_over.png'" onmouseout="Javascript:this.src='image/btn/btn_torna_alla_home.png'" style="cursor:pointer; padding-top:30px;"/></a>
+                <a href="index.php"><img alt="Homepage." src="image/btn/btn_torna_alla_home.png" onmouseover="this.src='image/btn/btn_torna_alla_home_over.png'" onmouseout="his.src='image/btn/btn_torna_alla_home.png'" style="cursor:pointer; padding-top:30px;"/></a>
                </div>
            
           </div>
